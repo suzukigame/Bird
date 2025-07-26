d6f8d283150a53cb5382b6a62566f19a58adbc6c
suzukigame <s.suzukigame@gmail.com>
2025-07-26 16:13:26 +0900

    "update"

--- my-bird-photos/src/data/photos.ts
+++ my-bird-photos/src/data/photos.ts
@@ -3,11 +3,21 @@ import { IPhoto } from '../types/photo';
 export const photos: IPhoto[] = [
   {
     id: '1',
-    src: '/images/birds/20250719_ibaraki_owl01.JPG',
-    thumbnail: '/images/birds/20250719_ibaraki_owl01.JPG',
+    src: process.env.PUBLIC_URL + '/images/birds/20250719_ibaraki_owl01.JPG',
+    thumbnail: process.env.PUBLIC_URL + '/images/birds/20250719_ibaraki_owl01.JPG',
     prefecture: '茨城県',
     date: '2025-07-19',
     birdSpecies: 'owl01',
+    locationDetail: '',
+    memo: '',
+  },
+  {
+    id: '2',
+    src: process.env.PUBLIC_URL + '/images/birds/20250717_ibaraki_asianparadiseflycatcher.JPG',
+    thumbnail: process.env.PUBLIC_URL + '/images/birds/20250717_ibaraki_asianparadiseflycatcher.JPG',
+    prefecture: '茨城県',
+    date: '2025-07-17',
+    birdSpecies: 'サンコウチョウ',
     locationDetail: '',
     memo: '',
   },
