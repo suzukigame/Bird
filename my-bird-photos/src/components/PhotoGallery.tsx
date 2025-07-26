02e0f11d8ad17b490e49c4512c4fea03b0dcff5a
suzukigame <s.suzukigame@gmail.com>
2025-07-27 07:39:49 +0900

    add photo zoomup

--- my-bird-photos/src/components/PhotoGallery.tsx
+++ my-bird-photos/src/components/PhotoGallery.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { IPhoto } from '../types/photo';
 
 interface IPhotoGalleryProps {
@@ -6,10 +6,20 @@ interface IPhotoGalleryProps {
 }
 
 const PhotoGallery: React.FC<IPhotoGalleryProps> = ({ photos }) => {
+  const [selectedImage, setSelectedImage] = useState<IPhoto | null>(null);
+
+  const handleImageClick = (photo: IPhoto) => {
+    setSelectedImage(photo);
+  };
+
+  const handleCloseModal = () => {
+    setSelectedImage(null);
+  };
+
   return (
     <div className="photo-gallery">
       {photos.map((photo) => (
-        <div key={photo.id} className="photo-item">
+        <div key={photo.id} className="photo-item" onClick={() => handleImageClick(photo)}>
           <img src={photo.thumbnail} alt={photo.birdSpecies} />
           <div className="photo-info">
             <h3>{photo.birdSpecies}</h3>
@@ -18,6 +28,21 @@ const PhotoGallery: React.FC<IPhotoGalleryProps> = ({ photos }) => {
           </div>
         </div>
       ))}
+
+      {selectedImage && (
+        <div className="modal-overlay" onClick={handleCloseModal}>
+          <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+            <img src={selectedImage.src} alt={selectedImage.birdSpecies} />
+            <button className="close-button" onClick={handleCloseModal}>&times;</button>
+            <div className="modal-info">
+              <h3>{selectedImage.birdSpecies}</h3>
+              <p>{selectedImage.prefecture} - {selectedImage.locationDetail}</p>
+              <p>{selectedImage.date}</p>
+              {selectedImage.memo && <p>{selectedImage.memo}</p>}
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
