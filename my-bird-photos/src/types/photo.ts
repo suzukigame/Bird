571730ac0cd5d6e3307c79f68dda66e301d69d4d
suzukigame <s.suzukigame@gmail.com>
2025-07-28 23:23:26 +0900

    add photos

--- my-bird-photos/src/types/photo.ts
+++ my-bird-photos/src/types/photo.ts
@@ -2,10 +2,10 @@ export interface IPhoto {
   id: string;
   src: string;
   thumbnail: string;
-  prefecture: string;
+  prefecture: { ja: string; en: string; zh: string; };
   date: string;
   birdSpecies: { ja: string; en: string; zh: string; };
-  locationDetail: { ja: string; en: string; zh: string; }; // Change to object
-  memo?: { ja: string; en: string; zh: string; }; // Change to object
-  family?: string;
+  locationDetail: { ja: string; en: string; zh: string; };
+  memo?: { ja: string; en: string; zh: string; };
+  family?: { ja: string; en: string; zh: string; };
 }
