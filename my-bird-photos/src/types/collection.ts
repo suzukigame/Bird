6c9df83dd9163d292c1934a7435306b571799979
suzukigame <s.suzukigame@gmail.com>
2025-07-28 19:29:35 +0900

    add zh string collection.ts

--- my-bird-photos/src/types/collection.ts
+++ my-bird-photos/src/types/collection.ts
@@ -1,11 +1,11 @@
 export interface ICollectionItem {
   id: string;
-  birdSpecies: { ja: string; en: string; };
+  birdSpecies: { ja: string; en: string; zh: string; };
   src: string;
   thumbnail: string;
   prefecture: string;
   date: string;
-  locationDetail: { ja: string; en: string; }; // 修正
-  memo?: { ja: string; en: string; }; // 追加
+  locationDetail: { ja: string; en: string; zh: string; }; // 修正
+  memo?: { ja: string; en: string; zh: string; }; // 追加
   family?: string;
 }
